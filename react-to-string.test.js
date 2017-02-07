d5778102f220dce4d5c391838ced7e5e1e7053cb
Kevin Huang <kevin.ziwen.huang@gmail.com>
2017-02-06 22:10:47 -0800

    Add simple react element test

--- react-to-string.test.js
+++ react-to-string.test.js
@@ -1,6 +1,12 @@
+import React from 'react';
 import reactToString from './react-to-string';
 
 test('Returns the original string if the element is a string', () => {
   const testString = 'This is a string';
   expect(reactToString(testString)).toBe(testString);
+});
+
+test('Returns the string child of an element that only has one child', () => {
+  const testElement = <div>This is a test string</div>
+  expect(reactToString(testElement)).toBe('This is a test string');
 });
