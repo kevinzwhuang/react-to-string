264f712a6ba06e766642838ff50b74cee0eee22b
Kevin Huang <kevin.ziwen.huang@gmail.com>
2017-02-06 22:15:48 -0800

    Add more complex react element test

--- react-to-string.test.js
+++ react-to-string.test.js
@@ -10,3 +10,12 @@ test('Returns the string child of an element that only has one child', () => {
   const testElement = <div>This is a test string</div>
   expect(reactToString(testElement)).toBe('This is a test string');
 });
+
+test('Returns only the string children of an element with several children', () => {
+  const testElement = (
+    <p>
+      Welcome to <a href="/about">reactToString</a>. <a href="/sign_up">Click here to sign up</a>
+    </p>
+  )
+  expect(reactToString(testElement)).toBe('Welcome to reactToString. Click here to sign up');
+});
